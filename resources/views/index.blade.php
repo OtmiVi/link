8faec8be9b8094d389925809be9288380b0cd193
OtmiVi <kyzivyra.3@gmail.com>
2022-07-15 19:18:44 +0300

    update page

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -9,20 +9,24 @@
 </head>
 <body>
     <div class="container">
-        <form action="#" method="post">
+        <form action="{{route('shortLink.store')}}" method="post">
+            @csrf
             <div class="input-group mt-5 mb-3">
-                <input type="text" class="form-control" placeholder="Put link">
+                <input type="text" name ='link' class="form-control" placeholder="Put link">
                 <input type="submit" value="submit" class="btn btn-outline-secondary">
             </div>
         </form>
+        @if(session('success'))
         <div class="alert alert-success d-flex justify-content-between">
             <div>A simple success alert—check it out!</div>
             <div class="stretched-link">X</div>
         </div>
+        @endif
         <div class="alert alert-danger d-flex justify-content-between">
             <div>A simple danger alert—check it out!</div>
             <div class="stretched-link">X</div>
         </div>
+        @if($links->count())
         <table class="table mt-3">
             <thead>
                 <tr>
@@ -31,24 +35,28 @@
                 </tr>
             </thead>
             <tbody>
+                @foreach($links as $link)
                 <tr>
-                    <th>ffef3</th>
-                    <th>test/com</th>
+                    <th>
+                        <a href="{{route('shortLink.redirect', $link->short_link)}}">
+                        {{$link->short_link}}
+                        </a>
+                    </th>
+                    <th>
+                        <a href="{{$link->link}}">
+                        {{$link->link}}
+                        </a>
+                    </th>
                 </tr>
-                <tr>
-                    <th>ffef3</th>
-                    <th>test/com</th>
-                </tr>
-                <tr>
-                    <th>ffef3</th>
-                    <th>test/com</th>
-                </tr>
-                <tr>
-                    <th>ffef3</th>
-                    <th>test/com</th>
-                </tr>
+                @endforeach
             </tbody>
         </table>
+        @else
+        <div class="alert alert-success d-flex justify-content-between">
+            <div>пусто</div>
+            <div class="stretched-link">X</div>
+        </div>
+    `   @endif
     </div>
     <script>
         const buttons = document.querySelectorAll(".stretched-link");
